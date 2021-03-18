66261e212e0af1b077f4a63ab773c988e4254f94
Alex Branton <ae.branton9@gmail.com>
2021-03-18 10:59:27 -0400

    TDD yet again
    made me realize that the question_index needed to be unique per session

--- app/models/trivia_session_question.rb
+++ app/models/trivia_session_question.rb
@@ -1,7 +1,8 @@
 class TriviaSessionQuestion < ApplicationRecord
-    # Not sure it should belong to a question...?
     belongs_to :trivia_session
     belongs_to :question
-    belongs_to :trivia_session_state
     validates :question_index, presence: true, :numericality => {:only_integer => true}
+    # We want to be sure that each trivia session has a running index, or at the very least,
+    # no index duplicates - otherwise we might show different questions to some users
+    validates_uniqueness_of :question_index, scope: :trivia_session_id
 end
--- test/models/trivia_session_question_test.rb
+++ test/models/trivia_session_question_test.rb
@@ -1,7 +1,39 @@
 require "test_helper"
 
 class TriviaSessionQuestionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    category = QuestionCategory.new(name: "Test")
+    @question = Question.new(question_category: category, question: "Willl these tests pass?")
+    answer = Answer.new(question: @question, answer: "I bloody hope so!")
+    player = Player.new(display_name: "Alex Branton", email: "ae.branton9@gmail.com")
+    trivia_session_state = TriviaSessionState.new(name: "Pending")
+    @trivia_session = TriviaSession.new(player: player, trivia_session_state: trivia_session_state, name: "Test Session")
+    @ts_question = TriviaSessionQuestion.new(trivia_session: @trivia_session, question: @question, question_index: 1)
+  end
+
+  test "trivia session question should be valid" do
+    assert @ts_question.valid?
+  end
+
+  test "trivia session question should have a linked question" do
+    @ts_question.question = nil
+    assert_not @ts_question.valid?
+  end
+
+  test "trivia session question should have a linked session" do
+    @ts_question.trivia_session = nil
+    assert_not @ts_question.valid?
+  end
+
+  test "trivia session question should have a question index" do
+    @ts_question.trivia_session = nil
+    assert_not @ts_question.valid?
+  end
+
+  test "trivia session question should have a unique question index" do
+    other = TriviaSessionQuestion.new(trivia_session: @trivia_session, question: @question, question_index: 1)
+    @ts_question.save
+    other.save
+    assert_not other.valid?
+  end
 end
