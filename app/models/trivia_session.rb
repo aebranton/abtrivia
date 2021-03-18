b790deb459acd7e89e78afb3eaa971211438419b
Alex Branton <ae.branton9@gmail.com>
2021-03-18 11:08:03 -0400

    test done for now
    finished model testing for the moment

--- app/models/trivia_session.rb
+++ app/models/trivia_session.rb
@@ -2,5 +2,5 @@ class TriviaSession < ApplicationRecord
     belongs_to :player
     belongs_to :trivia_session_state
     has_many :trivia_session_questions
-    validates :name, presence: true
+    validates :name, presence: true, length: {minimum: 5, maximum: 100}
 end
--- test/models/player_trivia_session_record_test.rb
+++ test/models/player_trivia_session_record_test.rb
@@ -1,7 +1,24 @@
 require "test_helper"
 
 class PlayerTriviaSessionRecordTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    player = Player.new(display_name: "Alex Branton", email: "ae.branton9@gmail.com")
+    trivia_session_state = TriviaSessionState.new(name: "Pending")
+    trivia_session = TriviaSession.new(player: player, trivia_session_state: trivia_session_state, name: "Test Session")
+    @pts_record = PlayerTriviaSessionRecord.new(player: player, trivia_session: trivia_session, victory: true)
+  end
+
+  test "player trivia session record should be valid" do
+    assert @pts_record.valid?
+  end
+
+  test "player trivia session record should be linked to a session" do
+    @pts_record.trivia_session = nil
+    assert_not @pts_record.valid?
+  end
+
+  test "player trivia session record should be linked to a player" do
+    @pts_record.player = nil
+    assert_not @pts_record.valid?
+  end
 end
