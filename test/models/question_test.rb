6192506c3cd2f580e9ca920683273478cf991ad8
Alex Branton <ae.branton9@gmail.com>
2021-03-17 21:05:31 -0400

    Added more tests
    tests for some key types. Will add more next time i start this up, then move on to playing around with pages.
    
    Once i have some basic db creations set up, im going to start on the actioncable setup.

--- test/models/question_test.rb
+++ test/models/question_test.rb
@@ -1,7 +1,26 @@
 require "test_helper"
 
 class QuestionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    category = QuestionCategory.new(name: "Test")
+    @question = Question.new(question_category: category, question: "Willl these tests pass?")
+  end
+
+  test "question text should exist" do
+    assert @question.valid?
+  end
+
+  test "question should not be too short" do
+    @question.question = "only some"
+    assert_not @question.valid?
+  end
+
+  test "question should not be too long" do
+    @question.question = "only some" * 50
+    assert_not @question.valid?
+  end
+
+  test "question should have a linked category" do
+    assert !!@question.question_category.name
+  end
 end
