e53425d5044dc9ccd2c6433935c724b8ba30eb7a
Alex Branton <ae.branton9@gmail.com>
2021-03-22 14:53:23 -0400

    Heroku tweak
    Been a while since ive used heroku... and never with action cable. This i think should do it

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,9 +43,9 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
-  config.action_cable.url = "wss://#{ENV['RAILS_HOST']}/cable"
+  config.action_cable.url = "wss://assurance-trivia.herokuapp.com/cable"
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-  config.action_cable.allowed_request_origins = [ ENV['RAILS_HOST'], /http:\/\/example.*/ ]
+  config.action_cable.allowed_request_origins = [ "http://assurance-trivia.herokuapp.com", "https://assurance-trivia.herokuapp.com/", /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
